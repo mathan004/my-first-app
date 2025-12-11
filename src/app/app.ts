ba17d462fd5e4bda599e3c2d1c9f7725c3017ce4
mathankumar-sparkout <mathankumar@sparkouttech.com>
2025-12-11 10:33:44 +0530

    added the data_binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-first-app');
+  title='my-first-app';
+
+  getTitle(){
+    return this.title;
+  }
+  //properties for image binding
+  imageUrl='https://angular.io/assets/images/logos/angular/angular.png';
+  imageAlt='Angular Logo';
+  
+  //safe navigation operator
+  user: any = {
+    name: 'John Doe',
+    address: {
+      street: '123 Main St',
+      city: 'Anytown'
+    }
+  };
+
+  //event binding
+
+  clickcount=0;
+  clickme(){
+    this.clickcount++;
+  }
+
+
+
 }
